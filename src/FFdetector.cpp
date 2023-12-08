0932d5abd30614f09ab15047d20c75abe1d91793
Rafal Szymura <raszymura@gmail.com>
2023-12-08 15:16:13 +0100

    First version of the simplified detector.

---
+++ src/FFdetector.cpp
@@ -0,0 +1,100 @@
+#include "FFdetector.h"
+
+FFdetector::FFdetector() 
+    : interrupt(false), duration(0.0) {}
+
+void FFdetector::setThresholds() {
+    /*
+    std::cout << "Set detector FF_Threshold values, in the range (-1, 1):\n";
+    while (1) {
+        std::cout << "-FF_Threshold: ";
+        std::cin >> threshold_minus;
+
+        if (threshold_minus < 1 && threshold_minus > -1) {
+            break;
+        }
+        else {
+            std::cout << "Incorrect data. Try again.\n";
+        }
+    }
+    while (1) {
+        std::cout << "+FF_Threshold: ";
+        std::cin >> threshold_plus;
+
+        if (threshold_plus > -1 && threshold_plus < 1) {
+            std::cout << "Incorrect data. Try again.\n";
+        }
+        else if (threshold_plus <= threshold_minus) {
+            std::cout << "Incorrect data. The upper threshold must be greater than the lower threshold.\n";
+        }
+        else {
+            break;
+        }
+    }
+    while (1) {
+        std::cout << "Enter your minimum detectable free fall duration, in the range (0, 10s), after which detection occurs: ";
+        std::cin >> minDuration;
+
+        if (minDuration > 0 && minDuration < 10) {
+            std::cout << "Incorrect data. Try again.\n";
+        }
+        else {
+            break;
+        }
+    }
+    std::cout << "---------------------------------------------------------------------------------\n";
+    std::cout << "Detected zone is from " << threshold_minus << "g to " << threshold_plus << "g\n";
+    std::cout << "Minimum triggering Free-Fall Duration is " << minDuration << "s\n";
+    */
+
+   // For testing purposes
+    minDuration = 0.6;
+    threshold_plus = 0.1;
+    threshold_minus = -0.1;
+}
+
+void FFdetector::setPeriod(IMURegisters& imuRegisters) {
+    samplingPeriod = 1.0 / imuRegisters.samplingRate;
+}
+
+void FFdetector::processReceivedData(const IMUData& imuData) {
+    simDuration += samplingPeriod;
+
+    // Entering the trigger range
+    bool inRange = isInRange(imuData.ax, threshold_minus, threshold_plus) &&
+                    isInRange(imuData.ay, threshold_minus, threshold_plus) &&
+                    isInRange(imuData.az, threshold_minus, threshold_plus);
+    //std::cout << inRange;
+
+    // Minimum duration in trigger range, when reached start measuring the duration of the free fall (trigger time is added)
+    interrupt = isFreeFallDetected(inRange);
+
+    if (interrupt) {
+        std::cout << "Free-Fall for: (" << imuData.ax << ", " << imuData.ay << ", " << imuData.az << ") for: " << duration << "s\n";
+        std::cout << "---------------------------------------------------------------------------------\n";
+    } //else {}
+}
+
+bool FFdetector::isFreeFallDetected(bool inRange) {
+    if (inRange) {
+        duration += samplingPeriod;
+    } else {
+        duration = 0;
+    }
+    
+    if (inRange && (duration >= minDuration) ) {
+        interrupt = 1;
+    } else {
+        interrupt = 0;
+    }
+
+    return interrupt;
+}
+
+bool FFdetector::isInRange(double value, double threshold_minus, double threshold_plus) {
+    return  ( (value >= threshold_minus) && (value <= threshold_plus) );
+}
+
+double FFdetector::getFreeFallDuration() const {
+    return duration;
+}
--- src/FFdetector.h
+++ src/FFdetector.h
@@ -1,1 +1,38 @@
 #pragma once
+
+#include "IMU.h"
+#include <iostream>
+
+class FFdetector {
+private:
+    bool interrupt;     // Flag true when minDuration is reached
+
+    double minDuration; // Minimum duration in trigger range
+    double duration;    // Full free-fall duration
+    double simDuration; // Full simulation duration
+
+    double threshold_plus;
+    double threshold_minus;
+    double samplingPeriod;
+
+    bool isInRange(double value, double threshold_minus, double threshold_plus);
+public:
+    FFdetector();
+
+    void setThresholds();
+    void setPeriod(IMURegisters& imuRegisters);
+    void processReceivedData(const IMUData& imuData);
+    bool isFreeFallDetected(bool inRange);
+    double getFreeFallDuration() const;
+};
+
+/*
+Description of variables, for more information go to the documentation.
+• Interrupt - flag, true when the IMU reaches the free fall state
+• minDuration - minimum time during which all 3-axes of the accelerometer are between 
+• Threshold_minus and Threshold_plus and which causes Interrupt to be triggered
+• Threshold_plus - upper (positive) limit of the free-fall zone
+• Threshold_minus - lower (negative) limit of the free-fall zone
+• Duration - duration of free fall
+*/
+
